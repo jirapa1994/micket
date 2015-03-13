d9b6e24ff5701437adc12d714b45c6d63996f379
jirapa1994 <jirapa.manow@gmail.com>
2015-03-13 14:58:44 +0700

    adddddd

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -3,7 +3,30 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window1(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([QPoint(170, 100), QPoint(50, 110), QPoint(132, 100), QPoint(100, 150)])
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 120, 100, 100, 0, 180 * 16)
+        p.drawPie(10, 120, 140, 120, 10, 80 * 10)
+
+        p.drawPolygon([QPoint(50, 200), QPoint(150, 20), QPoint(10, 40)])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
+
+class Simple_drawing_window2(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
@@ -19,14 +42,13 @@ class Simple_drawing_window(QWidget):
         p.drawPolygon([QPoint(1, 10), QPoint(5, 110), QPoint(12, 102), QPoint(10, 15)])
         p.setPen(QColor(255, 127, 0))
         p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 120, 100, 100, 0, 180 * 16)
+        p.drawPie(40, 120, 100, 100, 0, 180 * 16)
         p.drawPie(10, 120, 140, 120, 10, 80 * 10)
 
         p.drawPolygon([QPoint(50, 200), QPoint(150, 20), QPoint(10, 40)])
 
         p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
-
 
 class Simple_drawing_window3(QWidget):
     def __init__(self):
