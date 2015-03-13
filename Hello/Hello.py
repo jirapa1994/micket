efcf97c515a2c1ead45223286e5abac7884c9de4
jirapa1994 <jirapa.manow@gmail.com>
2015-03-13 15:52:44 +0700

    dfefe

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,30 +1,87 @@
-from tkinter import *
+from turtle import *
 
-canvas_width = 500
-canvas_height = 150
+class Disc(Turtle):
+    def __init__(self, n):
+        Turtle.__init__(self, shape="square", visible=False)
+        self.pu()
+        self.shapesize(1.5, n*1.5, 2) 
+        self.fillcolor(n/6., 0, 1-n/6.)
+        self.st()
 
-def paint( event ):
-   x1, y1 = ( event.x - 5 ), ( event.y - 5 )
-   x2, y2 = ( event.x + 5 ), ( event.y + 5 )
-   w.create_oval( x1, y1, x2, y2, fill = "#000000" )
-
-master = Tk()
-master.title( "A simple paint program" )
-w = Canvas(master, 
-           width=canvas_width, 
-           height=canvas_height)
-w.pack(expand = YES, fill = BOTH)
-w.bind( "<B1-Motion>", paint )
-
-def cls():
-    w.delete("all")
-
-clear = Button(master, text="Clear", command=cls)
+class Tower(list,Turtle):
+    def __init__(self, x):
+        
+        self.x = x
+    def push(self, d):
+        d.setx(self.x)
+        d.sety(-150+34*len(self))
+        self.append(d)
+    def pop(self):
+        d = list.pop(self)
+        d.sety(150)
+        return d
 
 
-message = Label( master, text = "Drag the mouse to draw" )
-message.pack( side = BOTTOM )
+def hanoi(n, from_, with_, to_):
+    if n > 0:
+        hanoi(n-1, from_, to_, with_)
+        to_.push(from_.pop())
+        hanoi(n-1, with_, from_, to_)
+
+
+ 
+   
+
+def main():
+    global t1, t2, t3
+    ht(); penup(); goto(0, -225)  
+    t1 = Tower(-250)
+    t2 = Tower(0)
+    t3 = Tower(250)
+    t = Turtle()
+    t.st()
+    t.pu()
+    t.goto(-260,-150)
+    t.pd()
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+
+    t.pu()
+    t.goto(-10,-150)
+    t.pd()
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+
+    t.pu()
+    t.goto(240,-150)
+    t.pd()
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+    t.fd(20)
+    t.left(90)
+    t.fd(300)
+    t.left(90)
+    for i in range(3,0,-1):
+        t1.push(Disc(i))
     
-mainloop()
+    hanoi(3, t1, t2, t3)
+    return "EVENTLOOP"
 
-  
+if __name__=="__main__":
+    
+    msg = main()
+    mainloop()
