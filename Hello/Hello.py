ec499f57b4f61514943628fdece6cc356aa39812
jirapa1994 <jirapa.manow@gmail.com>
2015-03-13 16:06:54 +0700

    bobobo

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,5 +1,5 @@
 from turtle import *
-#bobobobobobbiuhihodphbkerperkgpk
+#bobobobobobbiuhihodphbkerperkgpk orkew  final leaw na ja
 class Disc(Turtle):
     def __init__(self, n):
         Turtle.__init__(self, shape="square", visible=False)
