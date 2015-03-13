d401574b3961b69aedea718ba7e1c5e959c05c29
bhbo <boh406@gmail.com>
2015-03-13 14:35:57 +0700

    initial check-in

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -2,8 +2,8 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-
-class Simple_drawing_window(QWidget):
+#bo
+class Simple_drawing_window2(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
@@ -15,11 +15,12 @@ class Simple_drawing_window(QWidget):
 
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([QPoint(70, 100), QPoint(100, 110), QPoint(130, 100), QPoint(100, 150)])
+        p.drawPolygon([QPoint(170, 100), QPoint(50, 110), QPoint(132, 100), QPoint(100, 150)])
+        p.drawPolygon([QPoint(1, 10), QPoint(5, 110), QPoint(12, 102), QPoint(10, 15)])
         p.setPen(QColor(255, 127, 0))
         p.setBrush(qColor(255, 127, 0))
         p.drawPie(50, 100, 100, 0, 180 * 16)
 
-        p.drawPolygon([QPoint(50, 2), QPoint(150, 20), QPoint(10, 40)])
+        p.drawPolygon([QPoint(50, 21), QPoint(10, 20), QPoint(110, 40)])
 
         p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
